b4785c75d8fd08e14551f8a1b808ba86affc337c
Julia <jlilianacollins@gmail.com>
2020-03-16 08:52:40 -0700

    finished assessment

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -5,24 +5,24 @@
 
 # app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) controller holds the methods for the model BlogPost
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2)index calls colomns for all blogposts
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3)show calls coloumns for one blogpost found by its id
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) allows a form for a new blogpost to be made
   def new
   end
 
   def create
-    # 5)
+    # 5) allows for a new blog post to be created
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to @post
@@ -36,15 +36,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # 6)
+      # 6) redirect back to BlogPost.find(params[:id]) 
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 7)
+  # 7) private methods can only be used within the class
   private
   def blog_post_params
-    # 8)
+    # 8) says that when a new blogpost is created, it should have a title and content
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -53,8 +53,8 @@ end
 
 # app/models/blog_post.rb
 
-# 9)
+# 9) define the relationship between models
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10) comments belongs to blogpost and a blog post can have many comments
   has_many :comments
 end
